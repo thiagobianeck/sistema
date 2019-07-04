6a9dd65396047875c7edd31d67a4fe3c1f3c39b8
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-04 00:25:30 -0300

    21. Configurando el menú con VueJs

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -1,5 +1,175 @@
 
 @extends('principal')
 @section('contenido')
-    <example-component></example-component>
+    <template v-if="menu==0">
+        <example-component></example-component>
+    </template>
+
+    <template v-if="menu==1">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 1</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==2">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 2</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==3">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 3</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==4">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 4</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==5">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 5</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==6">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 6</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==7">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 7</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==8">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 8</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==9">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 9</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==10">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 10</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==11">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 11</h1>
+            </div>
+        </main>
+    </template>
+
+    <template v-if="menu==12">
+        <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <h1>Contenido del Menu 12</h1>
+            </div>
+        </main>
+    </template>
 @endsection
